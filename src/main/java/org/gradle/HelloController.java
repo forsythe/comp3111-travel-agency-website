c58bbbc5b145bdde690e12a76e2803d0592ee27d
forsythe <fjorsyth@gmail.com>
2017-09-30 14:33:27 +0800

    added autowiring

--- src/main/java/org/gradle/HelloController.java
+++ src/main/java/org/gradle/HelloController.java
@@ -1,13 +1,29 @@
 package org.gradle;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-	
+
+	@Autowired
+	private EmployeeRepository repo;
+
 	@RequestMapping("/")
 	public String index() {
 		return "it worked!";
+	}
+
+	@RequestMapping("/employees")
+	public String employeeList() {
+		String s = "";
+
+		for (Employee e : repo.findAll()) {
+			s += e.getName() + ": " + e.getSalary() + "\n";
+		}
+		return s;
 	}
 }
