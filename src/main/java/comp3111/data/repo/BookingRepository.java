d2d39303170e7f34bbbbf6d8a6195960f687ef3e
forsythe <fjorsyth@gmail.com>
2017-10-29 20:53:03 +0800

    fixed bcrypt rounds, so you can still login

--- src/main/java/comp3111/data/repo/BookingRepository.java
+++ src/main/java/comp3111/data/repo/BookingRepository.java
@@ -1,11 +1,14 @@
 package comp3111.data.repo;
 
+import java.util.Collection;
+
 import org.springframework.data.repository.CrudRepository;
 
 import comp3111.data.model.Booking;
-
-import java.util.Collection;
+import comp3111.data.model.Offering;
 
 public interface BookingRepository extends CrudRepository<Booking, Long> {
 	Collection<Booking> findByPaymentStatus(String paymentStatus);
+	
+	Collection<Booking> findByOffering(Offering o);
 }
