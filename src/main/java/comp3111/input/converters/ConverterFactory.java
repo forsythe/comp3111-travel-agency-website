8d5bc0b9d3a85e9a9bf1e652c069b0352047072c
forsythe <fjorsyth@gmail.com>
2017-11-03 01:17:42 +0800

    added child friendly attribute of tour, #44

--- src/main/java/comp3111/input/converters/ConverterFactory.java
+++ src/main/java/comp3111/input/converters/ConverterFactory.java
@@ -1,5 +1,6 @@
 package comp3111.input.converters;
 
+import com.vaadin.data.converter.StringToBooleanConverter;
 import com.vaadin.data.converter.StringToDoubleConverter;
 import com.vaadin.data.converter.StringToIntegerConverter;
 
@@ -10,5 +11,9 @@ public class ConverterFactory {
 
 	public static StringToIntegerConverter getStringToIntegerConverter() {
 		return new StringToIntegerConverter("must be an integer");
+	}
+
+	public static StringToBooleanConverter getStringToBooleanConverter() {
+		return new StringToBooleanConverter("must be true or false");
 	}
 }
