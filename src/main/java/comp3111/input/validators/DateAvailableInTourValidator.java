57de81a5db5df2877d3396c2729ecfb0259af65e
Kristian Suhartono <blackxephos2@gmail.com>
2017-11-18 20:03:59 +0800

    Javadocs for validators

--- src/main/java/comp3111/input/validators/DateAvailableInTourValidator.java
+++ src/main/java/comp3111/input/validators/DateAvailableInTourValidator.java
@@ -13,17 +13,34 @@ import java.util.Date;
 import static comp3111.input.validators.ReturnValidationErrorWithLogging.getValidationErrorLogged;
 
 /**
- * @author Forsythe
- *
+ * A validator to check whether a date is available for use in a tour.
+ * As in whether the date is legal for a certain to
+ * 
+ * @author Kristian Suhartono
+ * @version 1.0
+ * @since 2017-11-18
  */
 public class DateAvailableInTourValidator implements Validator<Date> {
 
 	private Tour tour;
 
+	/**
+	 * Constructor for the validator
+	 * @param tour This is the tour object that is going to use for validation
+	 */
 	DateAvailableInTourValidator(Tour tour) {
 		this.tour = tour;
 	}
 
+
+	/** 
+	 * Overrides the apply method in vaadin validators, checks whether the given value
+	 * is valid in the tour object that the validator has.
+	 * 
+	 * @param value The value that is going to be validated
+	 * @param context A value context for converters. Contains relevant information for converting values. 
+	 * @see com.vaadin.data.Validator#apply(java.lang.Object, com.vaadin.data.ValueContext)
+	 */
 	@Override
 	public ValidationResult apply(Date value, ValueContext context) {
 
--- src/main/java/comp3111/input/validators/ReturnValidationErrorWithLogging.java
+++ src/main/java/comp3111/input/validators/ReturnValidationErrorWithLogging.java
@@ -4,9 +4,21 @@ import com.vaadin.data.ValidationResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A class that logs all incidents of errors during validation
+ * @author kristiansuhartono
+ * @version 1.0
+ * @since 2017-11-18
+ *
+ */
 public class ReturnValidationErrorWithLogging {
 	private static final Logger log = LoggerFactory.getLogger(ReturnValidationErrorWithLogging.class);
 
+	/**
+	 * The function that logs the error to the logger.
+	 * @param msg The message that describes the error
+	 * @return
+	 */
 	static ValidationResult getValidationErrorLogged(String msg) {
 		log.info("Validation error: " + msg);
 		return ValidationResult.error(msg);
