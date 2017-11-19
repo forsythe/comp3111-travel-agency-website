f6148b69c4c265cf1d2063ca7095086b60ce6f57
Rex Cheng <hkchengad@connect.ust.hk>
2017-11-19 18:29:32 +0800

    k means look ok

--- src/main/java/comp3111/suggest/TourCluster.java
+++ src/main/java/comp3111/suggest/TourCluster.java
@@ -8,25 +8,29 @@ import comp3111.data.model.Offering;
 import comp3111.data.model.Tour;
 import comp3111.data.repo.*;
 import comp3111.input.auth.Authentication;
+import comp3111.input.validators.ValidatorFactory;
 import org.deeplearning4j.clustering.cluster.Cluster;
 import org.deeplearning4j.clustering.cluster.ClusterSet;
 import org.deeplearning4j.clustering.cluster.Point;
+import org.deeplearning4j.clustering.cluster.PointClassification;
 import org.deeplearning4j.clustering.kmeans.KMeansClustering;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.factory.Nd4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.TreeMap;
+import java.util.*;
 import java.util.stream.IntStream;
 
+/**
+ * A Class to cluster all the tours into clusters for the recommendation engine.
+ */
 @SpringComponent
 public class TourCluster {
 
-    private final static int clusterCount = 5;
-    private final static int maxIterationCount = 5;
+    private final static int clusterCount = 3;
+    private final static int maxIterationCount = 200;
     private final static String distanceMeasure = "cosinesimilarity";
 
     private CustomerRepository customerRepo;
@@ -34,6 +38,19 @@ public class TourCluster {
     private OfferingRepository offeringRepo;
     private TourRepository tourRepo;
 
+    private ClusterSet tourClusterSet;
+    private TreeMap<Long, Integer> customerID;
+    private TreeMap<String, ArrayList<Tour>> clusterIDToTour;
+
+    private static final Logger log = LoggerFactory.getLogger(TourCluster.class);
+
+    /**
+     * Create a tour cluster. All autowired.
+     * @param customerRepo Customer repo
+     * @param bookingRepo Booking repo
+     * @param offeringRepo Offering repo
+     * @param tourRepo Tour repo
+     */
     @Autowired
     public TourCluster(CustomerRepository customerRepo, BookingRepository bookingRepo,
                        OfferingRepository offeringRepo, TourRepository tourRepo){
@@ -43,47 +60,131 @@ public class TourCluster {
         this.tourRepo = tourRepo;
     }
 
+    /**
+     * Create a vector from customer for a tour. Each booking adds one to the customer vector.
+     * @param tour The tour to be looked at for creation
+     * @return float array with the size the same as the tree map
+     */
+    float[] getCustomerVector (Tour tour){
+        float customerVector[] = new float[customerID.size()];
+        Iterable<Offering> offeringsIterable = offeringRepo.findByTour(tour);
+        offeringsIterable.forEach(offering -> {
+            Iterable<Booking> bookingIterable = bookingRepo.findByOffering(offering);
+            bookingIterable.forEach(booking -> {
+                //Finding all the customers in all the offerings of a tour
+                try {
+                    int customerId = customerID.get(booking.getCustomer().getId());
+                    customerVector[customerId] += 1.0f;
+                }catch (Exception e){
+                    //Customer might not in the tree, that's ok
+                    log.info("Customer not in tree");
+                }
+            });
+        });
+        return customerVector;
+    }
+
+    /**
+     * Generate a treemap for all the customers in the repo to act as inverted index
+     */
+    private void generateCustomerTreeMap(){
+        //Re-labeling all the customers with IDs, acting as inverted index
+        customerID = new TreeMap<>();
+
+        Iterable<Customer> customerIterable = customerRepo.findAll();
+        ArrayList<Customer> tempList = new ArrayList<>();
+        customerIterable.forEach(tempList::add);
+        IntStream.range(0, tempList.size())
+                .forEach(index -> {
+                    customerID.put(tempList.get(index).getId(), index);
+                });
+    }
+
+    /**
+     * Generate the cluster set based on information in the database
+     */
     public void clusterTour(){
         KMeansClustering kmc = KMeansClustering.setup(clusterCount, maxIterationCount, distanceMeasure);
 
         List<Point> pointsLst = new ArrayList<>();
+        generateCustomerTreeMap();
 
-        //Re-labeling all the customers with IDs, acting as inverted index
-        TreeMap<Customer, Integer> customerID = new TreeMap<>();
         {
-            Iterable<Customer> customerIterable = customerRepo.findAll();
-            ArrayList<Customer> tempList = new ArrayList<>();
-            customerIterable.forEach(tempList::add);
+            Iterable<Tour> tourIterable = tourRepo.findAll();
+            tourIterable.forEach(tour -> {
+                float customerVector[] = getCustomerVector(tour);
+//            System.out.print(tour.getTourName());
+//            System.out.println(Arrays.toString(customerVector));
+                INDArray vector = Nd4j.create(customerVector);
+                Point pt = new Point(tour.getTourName(), vector);
+//            System.out.println(pt.getArray());
+                pointsLst.add(pt);
+            });
+        }
+
+        tourClusterSet = kmc.applyTo(pointsLst);
+        clusterIDToTour = new TreeMap<>();
+        List<Cluster> clusterList = tourClusterSet.getClusters();
+        for(Cluster c: clusterList) {
+            Point center = c.getCenter();
+            System.out.print(center.getId());
+            System.out.println(center.getArray());
+
+            clusterIDToTour.put(c.getId(), new ArrayList<>());
+        }
+
+        {
+            //Put all the tours into clusters
+            Iterable<Tour> tourIterable = tourRepo.findAll();
+            ArrayList<Tour> tempList = new ArrayList<>();
+            tourIterable.forEach(tempList::add);
             IntStream.range(0, tempList.size())
                     .forEach(index -> {
-                        customerID.put(tempList.get(index), index);
+                        clusterIDToTour.get(getClusterForTour(tempList.get(index)).getId()).add(tempList.get(index));
                     });
         }
+    }
+
+    ArrayList<Tour> getToursInCluster(Cluster c){
+        return clusterIDToTour.get(c.getId());
+    }
+
+    Cluster getClusterWithPoint(Point pt){
+        List<Cluster> clusterList = tourClusterSet.getClusters();
+        double max = 0.0;
+        Cluster maxCluster = clusterList.get(0);
+        for(Cluster c: clusterList) {
+            double dis = c.getDistanceToCenter(pt);
+            if (dis > max){
+                max = dis;
+                maxCluster = c;
+            }
+        }
+        return maxCluster;
+    }
+
+    /**
+     * Get the cluster that the tour belongs to
+     * @param tour The tour to be searched
+     * @return The cluster
+     */
+    private Cluster getClusterForTour(Tour tour){
+        float customerVector[] = getCustomerVector(tour);
+        INDArray vector = Nd4j.create(customerVector);
+        Point pt = new Point(tour.getTourName(), vector);
+        return getClusterWithPoint(pt);
+    }
+
+    /**
+     * Printing the tour clustering out. For Debugging.
+     */
+    public void printCluster(){
+        System.out.println("Cluster Classification:");
 
         Iterable<Tour> tourIterable = tourRepo.findAll();
         tourIterable.forEach(tour -> {
-            float customerVector[] = new float[customerID.size()];
-            Iterable<Offering> offeringsIterable = offeringRepo.findByTour(tour);
-            offeringsIterable.forEach(offering -> {
-                Iterable<Booking> bookingIterable = bookingRepo.findByOffering(offering);
-                bookingIterable.forEach(booking -> {
-                    //Finding all the customers in all the offerings of a tour
-                    int customerId = customerID.get(booking.getCustomer());
-                    customerVector[customerId] += 1.0f;
-                });
-            });
-            //Adding the vector to the point matrix
-            INDArray vector = Nd4j.create(customerVector);
-            pointsLst.add(new Point(tour.getTourName(), vector));
+            Cluster cluster = getClusterForTour(tour);
+            System.out.println(tour.getTourName() + " " + cluster.getId());
         });
-
-        ClusterSet cs = kmc.applyTo(pointsLst);
-        List<Cluster> clusterLst = cs.getClusters();
-
-        System.out.println("\nCluster Centers:");
-        for(Cluster c: clusterLst) {
-            Point center = c.getCenter();
-            System.out.println(center.getId());
-        }
     }
 }
--- src/test/java/unit/SuggestionTests.java
+++ src/test/java/unit/SuggestionTests.java
@@ -1,38 +1,270 @@
 package unit;
 
+import comp3111.Application;
+import comp3111.Utils;
 import comp3111.data.DBManager;
-import comp3111.data.repo.BookingRepository;
-import comp3111.data.repo.CustomerRepository;
-import comp3111.data.repo.OfferingRepository;
-import comp3111.data.repo.TourRepository;
+import comp3111.data.model.*;
+import comp3111.data.repo.*;
+import comp3111.suggest.SuggestTour;
 import comp3111.suggest.TourCluster;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.rmi.CORBA.Util;
+import java.awt.print.Book;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class SuggestionTests {
-    @Mock
+    @Autowired
+    SuggestTour suggestTour;
+    @Autowired
     TourRepository tourRepo;
-    @Mock
+    @Autowired
     CustomerRepository customerRepo;
-    @Mock
+    @Autowired
     BookingRepository bookingRepo;
-    @Mock
+    @Autowired
     OfferingRepository offeringRepo;
-    @Mock
+    @Autowired
     DBManager dbManager;
-    @InjectMocks
-    TourCluster tourCluster;
+    @Autowired
+    TourGuideRepository tourGuideRepo;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
+        dbManager.deleteAll();
+        populateDatabase();
+    }
+
+    @After
+    public void tearDown() {
+        dbManager.deleteAll();
+    }
+
+    private void createBookingForCustomer(Offering o, Customer c){
+        Booking b = new Booking();
+        b.setCustomer(c);
+        b.setOffering(o);
+        try {
+            dbManager.createNormalBookingForOffering(b);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    Offering sciOffering[] = new Offering[3];
+    Offering hisOffering[] = new Offering[3];
+    Offering shoOffering[] = new Offering[3];
+    private void populateDatabase(){
+        //Like science
+        Customer sciCustomer[] = new Customer[4];
+        for (int i=0; i<sciCustomer.length; i++){
+            sciCustomer[i] = new Customer();
+            customerRepo.save(sciCustomer[i]);
+        }
+
+        //Like history
+        Customer hisCustomer[] = new Customer[4];
+        for (int i=0; i<hisCustomer.length; i++){
+            hisCustomer[i] = new Customer();
+            customerRepo.save(hisCustomer[i]);
+        }
+
+        //Like shopping
+        Customer shoCustomer[] = new Customer[4];
+        for (int i=0; i<shoCustomer.length; i++){
+            shoCustomer[i] = new Customer();
+            customerRepo.save(shoCustomer[i]);
+        }
+
+        TourGuide tourGuide = new TourGuide();
+        tourGuideRepo.save(tourGuide);
+
+        //Science tour
+        Tour sciTour[] = new Tour[3];
+        for (int i=0; i<sciTour.length; i++){
+            sciTour[i] = new Tour();
+            sciTour[i].setAllowedDaysOfWeek(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7));
+            sciTour[i].setDays(1);
+        }
+
+        sciTour[0].setTourName("Science tour in HK");
+        sciTour[0].setDescription("This is a science trip for you to explore the space museum and the science museum" +
+                " and Observatory in Hong Kong. ");
+
+        sciTour[1].setTourName("One day trip to Mars");
+        sciTour[1].setDescription("Do you want to explore the beauty of space? Do you want to take a spaceship and travel" +
+                "to Mars in just one day? You must not miss this opportunity! Explore the cosmo with us!");
+
+        sciTour[2].setTourName("Rocket Design Workshop");
+        sciTour[2].setDescription("Everyone can be a rocket scientist! This is true rocket science, right in your face!" +
+                "Join our workshop, and you will have a chance to build your own spaceship and rocket in the HK" +
+                "science museum. ");
+
+        for (Tour t : sciTour){
+            tourRepo.save(t);
+        }
+
+        //History tour
+        Tour hisTour[] = new Tour[3];
+        for (int i=0; i<hisTour.length; i++){
+            hisTour[i] = new Tour();
+            hisTour[i].setAllowedDaysOfWeek(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7));
+            hisTour[i].setDays(1);
+        }
+
+        hisTour[0].setTourName("Visit the Hong Kong Museum of Coastal Defence");
+        hisTour[0].setDescription("Hong Kong Museum of Coastal Defence is a famous museum in Hong Kong. In the past, it" +
+                "was a coastal defense fortress. Back in 1941, the British army has defensed the Japanese army there." +
+                " It is a great place to learn more about the history of Hong Kong.");
+
+        hisTour[1].setTourName("Visit the Hong Kong History Museum");
+        hisTour[1].setDescription("The Hong Kong History Museum has a large collection of the history of Hong Kong. " +
+                "The exhibition includes Stone Age, times when Hong Kong was ruled by British and Japanese, and some" +
+                "more recent times history.");
+
+        hisTour[2].setTourName("Visit the Kowloon Walled City");
+        hisTour[2].setDescription("The Kowloon Walled City was a very chaotic place in Hong Kong back in 1940s. " +
+                "Since both the British and Chinese government have no control over this place, a lot of criminals, " +
+                "drug dealers hid in this place.");
+
+        for (Tour t : hisTour){
+            tourRepo.save(t);
+        }
+
+        //Shopping tour
+        Tour shoTour[] = new Tour[3];
+        for (int i=0; i<shoTour.length; i++){
+            shoTour[i] = new Tour();
+            shoTour[i].setAllowedDaysOfWeek(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7));
+            shoTour[i].setDays(1);
+        }
+
+        shoTour[0].setTourName("Shopping in Central");
+        shoTour[0].setDescription("Central is on of the best place in Hong Kong for you to shop! While there are a lot of " +
+                "high class clothing, jewelleries and watches, you can also enjoy 5-star restaurant if you are hungary! " +
+                "The only thing is that they are all expensive!");
+
+        shoTour[1].setTourName("Shopping in Diamond Hill");
+        shoTour[1].setDescription("Diamond Hill has a good mall! It is not as expensive as in Central, you can still have" +
+                "good quality electronic gadgets and clothing if you can pay the price!");
+
+        shoTour[2].setTourName("CRAZY SHOPPING IN HKUST");
+        shoTour[2].setDescription("You might not heard of it yet... In HKUST, they have this really good shop called the" +
+                "souvenir shop. They sell things that are essential to everyone's life like pens and water bottles" +
+                "at a perfectly reasonable price!");
+
+        for (Tour t : shoTour){
+            tourRepo.save(t);
+        }
+
+        int dateCounter = 100;
+        //Science offering
+        sciOffering = new Offering[sciTour.length];
+        for (int i=0; i<sciTour.length; i++){
+            sciOffering[i] = new Offering();
+            sciOffering[i].setStartDate(Utils.addDate(LocalDate.now(), dateCounter));
+            sciOffering[i].setTour(sciTour[i]);
+            sciOffering[i].setTourGuide(tourGuide);
+            sciOffering[i].setStatus(Offering.STATUS_PENDING);
+            sciOffering[i].setMinCustomers(0);
+            sciOffering[i].setMaxCustomers(100);
+            dateCounter += 2;
+        }
+
+        //History offering
+        hisOffering = new Offering[hisTour.length];
+        for (int i=0; i<hisTour.length; i++){
+            hisOffering[i] = new Offering();
+            hisOffering[i].setStartDate(Utils.addDate(LocalDate.now(), dateCounter));
+            hisOffering[i].setTour(hisTour[i]);
+            hisOffering[i].setTourGuide(tourGuide);
+            hisOffering[i].setStatus(Offering.STATUS_PENDING);
+            hisOffering[i].setMinCustomers(0);
+            hisOffering[i].setMaxCustomers(100);
+            dateCounter += 2;
+        }
+
+        //Shopping offerings
+        shoOffering = new Offering[shoTour.length];
+        for (int i=0; i<shoTour.length; i++){
+            shoOffering[i] = new Offering();
+            shoOffering[i].setStartDate(Utils.addDate(LocalDate.now(), dateCounter));
+            shoOffering[i].setTour(shoTour[i]);
+            shoOffering[i].setTourGuide(tourGuide);
+            shoOffering[i].setStatus(Offering.STATUS_PENDING);
+            shoOffering[i].setMinCustomers(0);
+            shoOffering[i].setMaxCustomers(100);
+            dateCounter += 2;
+        }
+
+        //Create offering for tour
+        try {
+            for (int i=0; i<sciOffering.length; i++){
+                dbManager.createOfferingForTour(sciOffering[i]);
+                dbManager.createOfferingForTour(hisOffering[i]);
+                dbManager.createOfferingForTour(shoOffering[i]);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //Create bookings for customers
+        for (int i=0; i<sciOffering.length; i++){
+            for (Customer c : sciCustomer){
+                createBookingForCustomer(sciOffering[i], c);
+            }
+            for (Customer c : hisCustomer){
+                createBookingForCustomer(hisOffering[i], c);
+            }
+
+            for (Customer c : shoCustomer){
+                createBookingForCustomer(shoOffering[i], c);
+            }
+        }
+
+        //Introduces some variances, well nothing is perfect
+        createBookingForCustomer(sciOffering[0], hisCustomer[0]);
+        createBookingForCustomer(hisOffering[1], shoCustomer[1]);
+        createBookingForCustomer(shoOffering[2], sciCustomer[2]);
     }
 
     @Test
     public void testTourCluster(){
-        tourCluster.clusterTour();
+        suggestTour.initCluster();
+
+        //Create a test customer for suggestion
+        Customer cus1 = new Customer();
+        customerRepo.save(cus1);
+        createBookingForCustomer(sciOffering[0], cus1);
+        List<Tour> sug1 = suggestTour.suggestForCustomer(cus1);
+        System.out.println(sug1);
+
+        //Create a test customer for suggestion
+        Customer cus2 = new Customer();
+        customerRepo.save(cus2);
+        createBookingForCustomer(hisOffering[1], cus2);
+        List<Tour> sug2 = suggestTour.suggestForCustomer(cus2);
+        System.out.println(sug2);
+
+        //Create a test customer for suggestion
+        Customer cus3 = new Customer();
+        customerRepo.save(cus3);
+        createBookingForCustomer(shoOffering[2], cus3);
+        List<Tour> sug3 = suggestTour.suggestForCustomer(cus3);
+        System.out.println(sug3);
     }
 }
